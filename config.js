feb22ad69bbd599683598f3079841684cdf18c5a
Abdiel Mendoza <abdielmemdoza2901@gmail.com>
2026-04-04 16:02:20 -0500

    arreglo

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 ﻿window.APP_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbw9Rhn575gmD27p4bETzAXHCZd79md6Y7eYluo0EEe7J2lO6G6cx0_bQaSyZs9auP4gaw/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzUAtQCwz-6bLDe5aeJOnNePwbdumZrAy3EOFaZc60oW00anK0QxO9XvBAtJqzakwlGIg/execc",
 };
